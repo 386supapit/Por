f5350530de23f984ce7c63bbb4f8cd61dca40e77
386supapit <supapitpumnin@gmail.com>
2024-10-14 13:07:55 +0700

    project

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,57 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import ImageSlider from './ImageSlider';
 import { Link } from 'react-router-dom'; 
+import park1 from './assets/1.jpg';
+import park2 from './assets/2.jpg';
+import park3 from './assets/3.jpg';
+import park4 from './assets/4.jpg';
+import park5 from './assets/5.jpg';
+import park6 from './assets/6.jpg';
+import park7 from './assets/7.jpg';
+import park8 from './assets/8.jpg';
+import park9 from './assets/9.jpg';
+import park10 from './assets/10.jpg';
 
 const parks = [
   {
-    name: 'อุทยานแห่งชาติดอยอินทนนท์',
-    image: '/inthanont.jpg',
+    name: 'อุทยานแห่งชาติดอยผ้าห่มปก',
+    image: park1,
   },
   {
     name: 'อุทยานแห่งชาติดอยสุเทพ-ปุย',
-    image: '/suthep-pui.jpg',
+    image: park2,
   },
   {
-    name: 'อุทยานแห่งชาติดอยผ้าห่มปก',
-    image: '/phahompok.jpg',
+    name: 'อุทยานแห่งชาติดอยอินทนนท์',
+    image: park3,
   },
   {
     name: 'อุทยานแห่งชาติออบหลวง',
-    image: '/obluang.jpg',
+    image: park4,
   },
   {
     name: 'อุทยานแห่งชาติห้วยน้ำดัง',
-    image: '/huainamdang.jpg',
+    image: park5,
   },
   {
-    name: 'อุทยานแห่งชาติเชียงดาว',
-    image: '/chiangdao.jpg',
+    name: 'อุทยานแห่งชาติขุนขาน',
+    image: park6,
   },
   {
-    name: 'อุทยานแห่งชาติดอยหลวง',
-    image: '/doiluang.jpg',
+    name: 'อุทยานแห่งชาติผาแดง',
+    image: park7,
   },
   {
-    name: 'อุทยานแห่งชาติดอยเวียงผา',
-    image: '/wiangpha.jpg',
+    name: 'อุทยานแห่งชาติศรีลานนา',
+    image: park8,
+  },
+  {
+    name: 'อุทยานแห่งชาติแม่ตะไคร้',
+    image: park9,
   },
   {
     name: 'อุทยานแห่งชาติแม่วาง',
-    image: '/maewang.jpg',
-  },
-  {
-    name: 'อุทยานแห่งชาติออบขาน',
-    image: '/obkhan.jpg',
+    image: park10,
   },
 ];
 
@@ -59,7 +69,7 @@ function Home() {
             <Link to={`/park/${park.name}`}>
               <img src={park.image} alt={park.name} className="w-full h-48 object-cover" />
               <div className="absolute bottom-0 left-0 right-0 p-2">
-              <h2 className="text-xl font-semibold text-black text-center">{park.name}</h2>
+              <h2 className="text-xl font-semibold text-white text-center">{park.name}</h2>
               </div>
             </Link>
           </div>
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -2,45 +2,41 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules'; // นำเข้าโมดูล Autoplay
 import 'swiper/swiper-bundle.css'; // นำเข้า CSS ของ Swiper
+ 
 
-const images = [
-  'https://th.bing.com/th/id/OIP.q7qOkpMuvA19fhj7qJzWkgHaEK?rs=1&pid=ImgDetMain',
-  'https://f.ptcdn.info/653/064/000/ptlxrdvxcsyOMvUtA6G-o.jpg',
-  'https://f.ptcdn.info/653/064/000/ptlxrdvxcsyOMvUtA6G-o.jpg',
-];
-
+const images = ['https://i0.wp.com/www.mychiangmaitravel.com/wp-content/uploads/2017/11/doi-ang-khang08.jpg?ssl=1',
+   'https://th.bing.com/th/id/OIP._tCNgLNyqYpicZxByMWNxgHaE7?pid=ImgDet&w=178&h=118&c=7&dpr=1.5', 
+   'https://th.bing.com/th/id/OIP._tCNgLNyqYpicZxByMWNxgHaE7?pid=ImgDet&w=178&h=118&c=7&dpr=1.5']; // ใช้ตัวแปรที่ import เข้ามา
 
 function ImageSlider() {
   return (
-    
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]} // ใช้โมดูล Autoplay
-        className="mySwiper"
-        breakpoints={{
-          640: {
-            slidesPerView: 1, // แสดง 1 รูป
-          },
-          768: {
-            slidesPerView: 1, // แสดง 1 รูป
-          },
-          1024: {
-            slidesPerView: 1, // แสดง 1 รูป
-          },
-        }}
-      >
-        {images.map((img, index) => (
-          <SwiperSlide key={index}>
-            <img src={img} alt={`Slide ${index}`} className="w-full h-96 object-cover" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]} // ใช้โมดูล Autoplay
+      className="mySwiper"
+      breakpoints={{
+        640: {
+          slidesPerView: 1, // แสดง 1 รูป
+        },
+        768: {
+          slidesPerView: 1, // แสดง 1 รูป
+        },
+        1024: {
+          slidesPerView: 1, // แสดง 1 รูป
+        },
+      }}
+    >
+      {images.map((img, index) => (
+        <SwiperSlide key={index}>
+          <img src={img} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
 
